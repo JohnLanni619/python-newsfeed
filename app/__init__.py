95d01ab4f803ec348bd2f69828ec5f85ffcd856b
John Lanni <johnlanni619@gmail.com>
2022-08-10 15:23:46 -0400

    removed strict url slashes for routing

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from app.routes import home, dashboard
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
